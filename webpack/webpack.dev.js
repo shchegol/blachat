c0e3d6f7693e27cbfb915a29085e5421834a436d
Щеголь Александр Анатольевич <zelenzoom@gmail.com>
2020-12-05 15:18:41 +0300

    edits

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,7 @@
 const { merge } = require('webpack-merge');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common.js');
-const util = require('./webpack.utils');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -35,17 +33,13 @@ module.exports = merge(common, {
     ],
   },
   devServer: {
-    // publicPath: '/',
     contentBase: './build',
     port: 4000,
     historyApiFallback: {
       index: './build/index.html',
     },
-    // compress: true,
-    // watchContentBase: true,
   },
   plugins: [
-    // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       filename: './index.html',
